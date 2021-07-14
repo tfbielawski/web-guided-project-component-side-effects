354505e4a519b0b1aa535ac5cc9509734014fc08
Tom Bielawski <70349084+tfbielawski@users.noreply.github.com>
2021-07-13 21:51:45 -0400

    This is a test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
+//Thomas Bielaswski
 // 👉 Importing React libs from node_modules folder
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 
 // 👉 Importing a React component from another file
-import App from './components/App'
+import App from "./components/App";
 
 // 👉 Importing our styles
-import './styles.less'
+import "./styles.less";
 
-render(
-  <App />,
-  document.querySelector('#root')
-)
+render(<App />, document.querySelector("#root"));
